f3d38ac373067c2f0be0b68f2830018c50794c12
Yeji-Yoon <angelayoon0102@gmail.com>
2025-03-27 08:13:54 -0700

    s

--- lib/debug.py
+++ lib/debug.py
@@ -2,7 +2,15 @@
 # lib/debug.py
 
 from models.__init__ import CONN, CURSOR
+from models.Course import Course
 import ipdb
 
+def reset_database():
+    Course.drop_table()
+    Course.create_table()
+
+    course = Course.create("Yeji", "A", "fall")
+
+reset_database()
 
 ipdb.set_trace()
